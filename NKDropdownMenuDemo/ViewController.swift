a949f8ceb0ecc27229df58764baa925e1bd602b6
Peng Guo <guoleii@gmail.com>
2015-11-17 12:19:07 +0800

    format the codes following swiftlint rules

--- NKDropdownMenuDemo/ViewController.swift
+++ NKDropdownMenuDemo/ViewController.swift
@@ -11,27 +11,25 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var selectedCellLabel: UILabel!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         let items = ["Most Popular", "Latest", "Trending", "Nearest", "Top Picks"]
-        
+
         self.selectedCellLabel.text = items.first
         self.navigationController?.navigationBar.translucent = false
-               
+
         let dropdownMenu: NKDropdownMenu = NKDropdownMenu(items: items)
         dropdownMenu.didSelectItemAtIndexHandler = {(indexPath: Int) -> () in
             print("Did select item at index: \(indexPath)")
             self.selectedCellLabel.text = items[indexPath]
-            
+
         }
-        
+
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: dropdownMenu)
-        
+
         self.view.backgroundColor = UIColor(red: 244.0 / 255.0, green: 244.0 / 255.0, blue: 244.0 / 255.0, alpha: 1.0)
     }
 
-  
 }
-
